41bf75d37471e49027f1fd9016f56849b50dd193
DaniyalAbbasi <36009198+abbasidaniyal@users.noreply.github.com>
2019-01-07 03:36:16 +0530

    save png missing

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
-import "JsBarcode.all.min.js
-"
 
+let save = function(id){
+    var svg = document.querySelector( "svg" );
+    var svgData = new XMLSerializer().serializeToString( svg );
+
+    var canvas = document.createElement( "canvas" );
+    var ctx = canvas.getContext( "2d" );
+
+    var img = document.createElement( "img" );
+    img.setAttribute( "src", "data:image/svg+xml;base64," + btoa( svgData ) );
+
+    img.onload = function() {
+        ctx.drawImage( img, 0, 0 );
+        var data= canvas.toDataURL( "image/png" )
+        console.log( data );
+       
+        path = "/";
+
+        //save data as png 
+
+};
+}
 
 let barCodeGen = function(id, type) {
     
     JsBarcode("#barcode", id, {
         format: type,
         lineColor: "#000000",
-        width: 1,
-        height: 40,
+        width: 2,
+        height: 80,
         displayValue: false
     });
 }
@@ -20,7 +39,6 @@ let go = function(){
     let format = "CODE128"
 
     barCodeGen(referenceId, format);
-
-    saveSvgAsPng(document.getElementById("barcode"), `${referenceId}.png`);
-
+    save(referenceId);
 }
+
